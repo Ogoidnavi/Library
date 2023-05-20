11cef4035247d87d5550900a6f930d918042d24b
Ogoidnavi <ogoidnavi13051997@gmail.com>
2023-05-20 12:20:31 +0100

    Add functionality to the cancel button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const addButton = document.querySelector(".add-book");
 const displayInfo = document.getElementById("book-modal");
+const cancel = document.querySelector("button[type = reset]");
+const deleteAll = document.getElementById("delete-modal");
 // let myLibrary = [];
 
 function Book(name, author, pages, read) {
@@ -9,7 +11,7 @@ function Book(name, author, pages, read) {
   this.read = read;
 }
 
-/* constructor manipulation practice 
+/* constructor manipulation practice (may remove if not necessary)
 Book.prototype.info = function () {
   if (this.read === true) {
     return `${this.name} by ${this.author}, ${this.pages} pages, has been read`;
@@ -21,6 +23,15 @@ addButton.addEventListener("click", () => {
   displayInfo.classList.remove("display-none");
 });
 
+cancel.addEventListener("click", () => {
+  if (!displayInfo.classList.contains("display-none")) {
+    displayInfo.classList.add("display-none");
+  }
+  if (!deleteAll.classList.contains("display-none")) {
+    deleteAll.classList.add("display none");
+  }
+});
 /* TODO
 Add blank modal to html, then add all the data that the user inputs through javascript
+Add function to make delete-modal appear
 */
