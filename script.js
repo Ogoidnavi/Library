2a897a44ba921eb8701963fcffa45aa340992003
Ogoidnavi <ogoidnavi13051997@gmail.com>
2023-05-22 13:35:40 +0100

    Start the function to add books to the library Style and animate delete all books and sign in buttons Change the code on cancel-like buttons due to errors

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const addButton = document.querySelector(".add-book");
+const deleteButt = document.getElementById("delete-books");
+const cancel = document.querySelectorAll("button[type = reset]");
 const displayInfo = document.getElementById("book-modal");
-const cancel = document.querySelector("button[type = reset]");
 const deleteAll = document.getElementById("delete-modal");
-// let myLibrary = [];
+const yesButt = document.getElementById("yes");
+const submitButt = document.getElementById("submitButt");
+const bookForm = document.getElementById("bookForm");
+
+let myLibrary = [];
 
 function Book(name, author, pages, read) {
   this.name = name;
@@ -23,14 +28,32 @@ addButton.addEventListener("click", () => {
   displayInfo.classList.remove("display-none");
 });
 
-cancel.addEventListener("click", () => {
-  if (!displayInfo.classList.contains("display-none")) {
-    displayInfo.classList.add("display-none");
-  }
-  if (!deleteAll.classList.contains("display-none")) {
-    deleteAll.classList.add("display none");
-  }
+deleteButt.addEventListener("click", () => {
+  deleteAll.classList.remove("display-none");
 });
+
+/* If more cancel-like buttons are created, using a case switch should be considered */
+cancel.forEach((occurrence) => {
+  occurrence.addEventListener("click", () => {
+    if (!displayInfo.classList.contains("display-none")) {
+      displayInfo.classList.add("display-none");
+    }
+    if (!deleteAll.classList.contains("display-none")) {
+      deleteAll.classList.add("display-none");
+    }
+  });
+});
+
+yesButt.addEventListener("click", () => {
+  myLibrary = [];
+});
+
+bookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // handle submit
+});
+
 /* TODO
 Add function to make delete-modal appear
 Add blank modal to html, then add all the data that the user inputs through javascript
