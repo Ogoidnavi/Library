2e49918e788b33c3c18eeb096b73bb8ad2c3759a
Ogoidnavi <ogoidnavi13051997@gmail.com>
2023-06-03 16:39:26 +0100

    Create the beginning of the book modal Correct code on the submit button

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const cancel = document.querySelectorAll("button[type = reset]");
 const displayInfo = document.getElementById("book-info-modal");
 const deleteAll = document.getElementById("delete-modal");
 const yesButt = document.getElementById("yes");
-const submitButt = document.getElementById("submitButt");
 const bookForm = document.getElementById("bookForm");
 
 let myLibrary = [];
@@ -16,13 +15,15 @@ function Book(name, author, pages, read) {
   this.read = read;
 }
 
-/* constructor manipulation practice (may remove if not necessary)
 Book.prototype.info = function () {
-  if (this.read === true) {
-    return `${this.name} by ${this.author}, ${this.pages} pages, has been read`;
-  }
-  return `${this.name} by ${this.author}, ${this.pages} pages, not read yet`;
-}; */
+  const bookRead = document.getElementById("firstCheck");
+  const checkLabel = document.getElementById("firstLabel");
+  bookRead.addEventListener("click", () => {
+    if (!bookRead.checked) {
+      checkLabel.innerHTML = "Not Read";
+    }
+  });
+};
 
 addButton.addEventListener("click", () => {
   displayInfo.classList.remove("display-none");
@@ -46,6 +47,7 @@ cancel.forEach((occurrence) => {
 
 yesButt.addEventListener("click", () => {
   myLibrary = [];
+  displayLibrary();
 });
 
 bookForm.addEventListener("submit", (e) => {
@@ -54,7 +56,7 @@ bookForm.addEventListener("submit", (e) => {
   const bookName = document.getElementById("name");
   const author = document.getElementById("author");
   const numberPages = document.getElementById("pages");
-  const beenRead = document.getElementById("check");
+  const beenRead = document.getElementById("secondCheck");
 
   const myBook = new Book(
     bookName.value,
@@ -64,11 +66,49 @@ bookForm.addEventListener("submit", (e) => {
   );
 
   myLibrary.push(myBook);
+  displayInfo.classList.add("display-none");
   displayLibrary();
 });
 
-function displayLibrary() {}
+function createBookModal() {
+  const modal = document.createElement("div");
+  modal.setAttribute("class", "book-modal");
+  modal.setAttribute("id", "book-modal");
 
+  /* creation of the modal container div */
+  const modalContainer = document.createElement("div");
+  modalContainer.setAttribute("class", "modal-container");
+  modal.appendChild(modalContainer);
+
+  /* creation of the modal content div */
+  const modalContent = document.createElement("div");
+  modalContent.setAttribute("class", "modal-content");
+  modalContainer.appendChild(modalContent);
+
+  /* creation of the informational divs */
+  const firstDiv = document.createElement("div");
+  firstDiv.setAttribute("class", "bookName");
+  modalContent.appendChild(firstDiv);
+
+  const secondDiv = document.createElement("div");
+  secondDiv.setAttribute("class", "bookAuthor");
+  modalContent.appendChild(secondDiv);
+
+  const firstDiv = document.createElement("div");
+  firstDiv.setAttribute("class", "bookName");
+  modalContent.appendChild(firstDiv);
+
+  const firstDiv = document.createElement("div");
+  firstDiv.setAttribute("class", "bookName");
+  modalContent.appendChild(firstDiv);
+
+  /* Insert modal before the add-book button */
+  const bookDisplay = document.querySelector(".book-display");
+  bookDisplay.insertBefore(modal, addButton);
+}
+
+function displayLibrary() {}
 /* TODO
+Continue the creation of the bookModal
 Add blank modal to html, then add all the data that the user inputs through javascript
 */
